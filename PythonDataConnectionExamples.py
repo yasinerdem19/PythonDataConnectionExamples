103325a7152c76887ffe80972e7f26b88dcc6fef
Trudy-Yasin Erdem <trudyasin@outlook.com>
2022-11-01 00:08:40 +0300

    Add project files.

---
+++ PythonDataConnectionExamples.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+url= "https://people.sc.fsu.edu/~jburkardt/data/csv/cities.csv"
+df=pd.read_csv(url)
+headers=["aaaaa","bbbb","c","d","e","f","g","h","i","j"]
+
+#df.columns=headers
+
+print(df.head(2))
+#print(df.dtypes)
+#print(df.describe(include='all'))
+print(df.info)
+
+
+
